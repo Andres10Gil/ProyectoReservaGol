7705513920fdcf0ac1e9e3b77cbf06dbe7925f2e
Andres Felipe Gil <andres11.graff@gmail.com>
2025-12-08 12:22:50 -0500

    controladores factutracion

---
+++ ReservaGol/Repositorios/Interfaces/IPagosRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ReservaGol.Modelos;
+
+namespace ReservaGol.Repositorios.Interfaces
+{
+    public interface IPagosRepositorio
+    {
+      
+        
+            Task<List<Pagos>> ObtenerPagos(); // contrato tipo de documento
+
+            Task<Pagos> ObtenerPagos(Guid id);
+
+
+
+ 
+        }
+    }
+
+
